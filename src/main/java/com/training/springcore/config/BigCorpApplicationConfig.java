fe9624eb4cecdf2e5e6f34dd29297e02c4171057
Lisa <maestrilisa@yahoo.fr>
2019-03-11 13:57:58 +0100

    tp 7 profil et resources

---
+++ src/main/java/com/training/springcore/config/BigCorpApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.training.springcore.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan
+@Configuration
+
+public class BigCorpApplicationConfig {
+
+
+   }
